5403eacacd93f6d0727bf755038ae8af2507b989
Oluwafemi <femio82@gmail.com>
2025-03-28 20:02:32 +0100

    configured graphql module

---
+++ apps/scheduling-service/src/config/graphql.config.ts
@@ -0,0 +1,18 @@
+import { ApolloDriver, ApolloDriverConfig} from '@nestjs/apollo'
+import {Module} from '@nestjs/common'
+import {GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path'
+
+@Module({
+    imports: [
+        GraphQLModule.forRoot<ApolloDriverConfig>({
+            driver: ApolloDriver,
+            autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+            sortSchema: true,
+            playground: process.env.NODE_ENV !== 'production',
+            introspection: process.env.NODE_ENV !== 'production',
+        })
+    ]
+})
+
+export class GraphqlConfigModule {}
