4dcdf3986e6be63b77d465c89a0cecdc325b7dcd
Oluwafemi <femio82@gmail.com>
2025-03-29 20:00:15 +0100

    Added calender dto

---
+++ apps/scheduling-service/src/modules/calenders/dto/create-calender.input.ts
@@ -0,0 +1,21 @@
+import {Field, InputType} from '@nestjs/graphql'
+
+import {IsNotEmpty, IsString, MaxLength} from 'class-validator'
+
+@InputType()
+export class CreateCalenderInput{
+    @Field()
+    @IsString()
+    @IsNotEmpty()
+    @MaxLength(100)
+    name: string;
+
+    @Field({nullable: true})
+    @IsString()
+    @MaxLength(500)
+    description?: string;
+
+    @Field({defaultValue: "#4285F4"})
+    @IsString()
+    color?: string
+}
---
+++ apps/scheduling-service/src/modules/calenders/models/calender.model.ts
@@ -0,0 +1,33 @@
+import {Field, ID, ObjectType} from '@nestjs/graphql'
+import {User} from '../../users/models/user.model'
+import {Event } from '../../events/models/event.model'
+
+@ObjectType()
+export class Calender{
+    @Field(() => ID)
+    id: string
+
+    @Field()
+    name: string;
+
+    @Field()
+    description: string
+
+    @Field()
+    color: string
+
+    @Field(()=> User)
+    owner: User
+
+    @Field()
+    ownerId: string
+
+    @Field(()=> [Event], {nullable: true})
+    events?: Event[]
+
+    @Field()
+    createdAt: Date
+
+    @Field()
+    updatedAt: Date
+}
